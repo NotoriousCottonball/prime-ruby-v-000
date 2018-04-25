6b6101017a9a9c4a91fd9bca20e5bfe6495e9267
George  Pianka <gpianka1@gmail.com>
2018-04-25 09:57:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
+# If int is not prime, it can be factored into two factors: a, b.
+# if both a & b were > square_root of int, then a*b > int. 
+# So, at least one of the factors must be < or equal to square_root of int.
+
+
+
 def prime?(int)
+  
