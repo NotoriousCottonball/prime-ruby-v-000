f5ed17bbdf21ee76ab63653b928c32ce10e3b464
George  Pianka <gpianka1@gmail.com>
2018-04-25 10:03:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,11 @@
 
 
 
-def prime?(int)
-  return false if int <= 1 
+#def prime?(int)
+  #return false if int <= 1 
   
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
